5771854d300e008cd49be6353a61f4212435e7b4
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2017-05-20 20:53:10 -0300

    Possibilidade de editar dados do cliente.

--- ts/area_usuario.js
+++ ts/area_usuario.js
@@ -1,12 +1,34 @@
 ///<reference path="Server.ts"/>
 var server = new Server();
-function editInfo() {
+function refreshUserData() {
+    $(".clientData").each(function () {
+        let field_name = $(this).attr("id");
+        $(this).html(server.data[field_name]);
+    });
 }
 $(document).ready(function () {
     $(window).unload(() => server.saveState()); // para salvar o estado do servidor mock ao sair da página
-    $(".editInfo").click(() => {
-        console.log("Oi");
-        //console.log($(this).siblings("span"))
-        $(this).hide();
+    refreshUserData();
+    $(".editInfo").css("cursor", "pointer"); // cursor de link
+    $(".editInfo").click(function () {
+        let editButton = $(this);
+        editButton.hide();
+        let field = editButton.prev(); // sibling anterior (contém o dado atual do usuário)
+        field.hide();
+        let updateInputField = $("<input type=\"text\"></input>"); // cria novo elemento input	
+        updateInputField.val(field.html()); // inicializa o valor do element input com o valor do dado atual
+        updateInputField.blur(function () {
+            server.data[field.attr("id")] = $(this).val(); // o id de field tem o mesmo nome que o atributo correspondente no servidor
+            refreshUserData();
+            field.show();
+            $(this).remove();
+            editButton.show();
+        });
+        updateInputField.keypress(function (e) {
+            if (e.keyCode == 13)
+                $(this).blur();
+        });
+        field.after(updateInputField);
+        updateInputField.focus();
     });
 });
