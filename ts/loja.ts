2d1ef086ed03ad193458406c821421fa2d781a56
Arnaldo <nstanzani04@gmail.com>
2017-06-09 15:26:54 -0300

    Adicionado mais produtos, soma dos preços e itens do carrinho e troca de páginas

--- ts/loja.ts
+++ ts/loja.ts
@@ -4,14 +4,43 @@
 declare var $: any;
 var server: Server = new Server()
 
+let cartProducts: Product[] = [];
+let currentPage: number = 1;
+
 function changePage(page: number) : void
 {
 
+	if (page == 0)
+		currentPage = 1;
+	else
+		currentPage = page;
+	
+	if (page == 0){
+		if (localStorage.currPage){
+			currentPage = JSON.parse(localStorage.currPage);
+		}
+	}
+
+	localStorage.currPage = JSON.stringify(currentPage);
+
+	/*if (localStorage.currPage){
+		currentPage = JSON.parse(localStorage.currPage);
+	}
+	else{
+		currentPage = page;
+	}*/
+
+
+	$("a[href='#top']").click(function() {
+  		$("html, body").animate({ scrollTop: 100 }, "slow");
+  		return false;
+	});
+
 	let i: number = 0;
-	let p: number = (page-1) * 9;
+	let p: number = (currentPage-1) * 9;
 
 	$(".active").attr("class", "");
-	$("#page" + page).attr("class", "active");
+	$("#page" + currentPage).attr("class", "active");
 
 	for (i = 1; i <= 9; i++){
 		$(".product" + i).each(function()
@@ -40,4 +69,21 @@ function changePage(page: number) : void
 			}
 		})
 	}
+}
+
+function cart(pos: number)
+{
+	let p: number = (currentPage-1) * 9;
+	let sum: number = 0;
+	let n: number = 0;
+
+	cartProducts.push(server.products[pos-1+p]);
+
+	cartProducts.forEach(product => {
+		sum += product.price;
+		n++;
+	});
+
+	$("#cart_price").html("R$" + sum.toFixed(2).replace(".", ","));
+	$("#cart_qtd").html(n.toString() + " itens");
 }
